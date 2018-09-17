8d2779be78ca14ff830895655cede5d4958cadad
Jasmine Humbert <jasmine.yhumbert@gmail.com>
2018-09-17 16:17:02 -0700

    I think this basically rolls back to commit 57977294d233ed8c1c0fd20abc84a8647d0b8357

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser');
+const methodOverride = require('method-override')
 
 const exphbs = require('express-handlebars');
+
+app.use(methodOverride('_method'))
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
@@ -13,9 +16,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const mongoose = require('mongoose');
 
 // Copied this from Raymond's project
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/rotten-potatoes-jasmine-h', {useNewUrlParser: true})
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/rotten-potatoes', {useNewUrlParser: true})
 .then(() => {
-    console.log("Database connection success!");
+    console.log("Connected to DB");
 })
 .catch( err => {
     throw err;
@@ -53,6 +56,43 @@ app.listen(3000, () => {
 //add
 app.get('/reviews/new', (req, res) => {
   res.render('reviews-new', {});
+})
+
+// EDIT
+app.get('/reviews/:id/edit', function (req, res) {
+  Review.findById(req.params.id, function(err, review) {
+    res.render('reviews-edit', {review: review});
+  })
+})
+
+// SHOW
+app.get('/reviews/:id', (req, res) => {
+  Review.findById(req.params.id).then((review) => {
+    res.render('reviews-show', { review: review })
+  }).catch((err) => {
+    console.log(err.message);
+  })
+})
+
+// UPDATE
+app.put('/reviews/:id', (req, res) => {
+  Review.findByIdAndUpdate(req.params.id, req.body)
+    .then(review => {
+      res.redirect(`/reviews/${review._id}`)
+    })
+    .catch(err => {
+      console.log(err.message)
+    })
+})
+
+// DELETE
+app.delete('/reviews/:id', function (req, res) {
+  console.log("DELETE review")
+  Review.findByIdAndRemove(req.params.id).then((review) => {
+    res.redirect('/');
+  }).catch((err) => {
+    console.log(err.message);
+  })
 })
 
 app.post('/reviews', (req, res) => {
