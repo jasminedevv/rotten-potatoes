95afcc26f60b045c9206565be276263353c65d12
Jasmine Humbert <jasmine.yhumbert@gmail.com>
2018-09-25 18:24:45 -0700

    end of day commit

--- controllers/comments.js
+++ controllers/comments.js
@@ -1,18 +1,16 @@
 const Comment = require('../models/comment')
 
 module.exports = (app) => {
-
-    // NEW
+// CREATE Comment
     app.post('/reviews/comments', (req, res) => {
-        Comment.create(req.body)
-        .then(comment => {
-            console.log(comment);
-            console.log(comment.reviewId);
-          res.redirect(`/reviews/${comment.reviewId._id}`);
-        }).catch((err) => {
-          console.log(err.message);
-        });
-      });
+      Comment.create(req.body).then(comment => {
+        console.log("comment registered with content: "  ,req.body);
+        res.status(200).send({ comment: comment });
+        console.log("I have responded with content: ", comment);
+      }).catch((err) => {
+        res.status(400).send({ err: err })
+      })
+    })
 
     // DELETE
     app.delete('/reviews/comments/:id', function (req, res) {
