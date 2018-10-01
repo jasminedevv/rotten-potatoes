158dae427c1682be200803db4bff8877c735f786
Jasmine Humbert <jasmine.yhumbert@gmail.com>
2018-09-30 23:26:11 -0700

    fixed the delete bug with the admin dash

--- public/javascript/scripts.js
+++ public/javascript/scripts.js
@@ -39,9 +39,8 @@ if (el) {
     });
 }
 
-elementsArray = document.querySelectorAll('[id^="deleteComment-"]');
-console.log(elementsArray);
-elementsArray.forEach(function (elem) {
+var commentDeleteButtons = document.querySelectorAll('[id^="deleteComment-"]');
+commentDeleteButtons.forEach( (elem) => {
     elem.addEventListener('click', (e) => {
         console.log("click!");
         let commentId = elem.getAttribute('data-comment-id');
@@ -53,6 +52,26 @@ elementsArray.forEach(function (elem) {
                 comment.parentNode.removeChild(comment);
             })
             .catch(error => {
+                console.log(error);
+            });
+    });
+});
+
+var reviewDeleteButtons = document.querySelectorAll('[class^="deleteReview"]');
+reviewDeleteButtons.forEach( (elem) => {
+    console.log("There should b lots of me");
+    elem.addEventListener('click', (e) => {
+        console.log("click registered");
+        let reviewId = elem.getAttribute('data-review-id');
+        console.log(reviewId);
+        axios.delete(`/admin/delete/${reviewId}`)
+            .then(httpResponse => {
+            console.log(httpResponse);
+            review = document.getElementById(reviewId);
+            review.parentNode.removeChild(review);
+            })
+            .catch( error => {
+                console.log("im in the error block");
                 console.log(error);
             });
     });
